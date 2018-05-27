1033bab373c8786fe510a184b156d966fccb97da
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:31 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-import mapValues from "@unction/mapvalues"
-import thrush from "@unction/thrush"
-
-type MapFunctorType = ArrayType<MapFunctionType> | SetType<MapFunctionType> | ObjectType<ObjectKeyType, MapFunctionType> | MapType<MapKeyType, MapFunctionType>
-
-export default function sequence (unctions: MapFunctorType): Function {
-  return function sequenceUnctions (value: mixed): ArrayType | SetType | ObjectType | MapType {
-    return mapValues(
-      thrush(value)
-    )(
-      unctions
-    )
-  }
+import mapValues from "@unction/mapvalues";
+import thrush from "@unction/thrush";
+export default function sequence(unctions) {
+  return function sequenceUnctions(value) {
+    return mapValues(thrush(value))(unctions);
+  };
 }
