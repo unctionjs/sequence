d3d7dec4eaaf7a673738eb70406d11a654fbf62e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:53 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-magic-numbers */
+import sequence from "./index.ts";
+
+const increment = (value) => value + 1;
+const decrement = (value) => value - 1;
+const itself = (value) => value;
+
+
+test(() => {
+  expect(sequence([increment, decrement])(1)).toEqual([2, 0]);
+});
+
+test(() => {
+  expect(sequence(new Set([increment, decrement]))(1)).toEqual(new Set([2, 0]));
+});
+
+test(() => {
+  expect(sequence(new Map([["a", increment], ["b", decrement]]))(1)).toEqual(new Map([["a", 2], ["b", 0]]));
+});
+
+test(() => {
+  expect(sequence({
+    xxx: increment,
+    yyy: decrement,
+    zzz: itself,
+  })(1)).toEqual({
+    xxx: 2,
+    yyy: 0,
+    zzz: 1,
+  });
+});
